2d90f601bc3f0413dd68d323639ca68ff20549b1
Vũ Quang Hào <hao15845@gmail.com>
2026-08-12 21:32:18 +0700

    upd: somehow

--- src/hao1337/addins/AutoUpdate.java
+++ src/hao1337/addins/AutoUpdate.java
@@ -111,7 +111,7 @@ public class AutoUpdate {
 					if (!allowPrerelease && version.getBool("prerelease", false))
 						return false;
 					String tag = version.getString("tag_name");
-					return Version.isLargerOrEqual(tag);
+					return Version.isLarger(tag);
 				} catch (InvalidVersionStringException e) {
 					e.printStackTrace();
 					return false;
